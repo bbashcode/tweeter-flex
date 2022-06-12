a88f18d9e1c00fba4d428bceceebad2713374f92
bbashcode <binbash.code@gmail.com>
2022-06-12 22:57:20 +0000

    Submit form using jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -58,27 +58,16 @@ const renderTweets = function(tweets) {
 
 $(() => {
   renderTweets(data);
+
+  $('.new-tweet-from-form').on("submit", (event)=>{
+    event.preventDefault();
+    const data = $('.new-tweet-from-form').serialize();
+
+    console.log(data);
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: data
+    })
+  });
 });
-
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
-
-// $(() => {
-//   const $tweet = createTweetElement(tweetData);
-
-//   // Test / driver code (temporary)
-//   console.log($tweet); // to see what it looks like
-//   $('.tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements,
-// });
