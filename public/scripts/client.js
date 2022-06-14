d5e49bdc34e4f25765a82240fb6a238715756adf
bbashcode <binbash.code@gmail.com>
2022-06-14 04:18:18 +0000

    Remove hard-coded tweets objects

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,4 @@
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
 
 const createTweetElement = function(tweet) {
   const $tweet = `
@@ -57,8 +33,6 @@ const renderTweets = function(tweets) {
 
 
 $(() => {
-  renderTweets(data);
-
   $('.new-tweet-from-form').on("submit", (event)=>{
     event.preventDefault();
     const data = $('.new-tweet-from-form').serialize();
@@ -71,6 +45,8 @@ $(() => {
     })
   });
 
+  renderTweets(data);
+
   const loadTweets = () => {
     $.ajax({
       method: "GET",
@@ -80,5 +56,6 @@ $(() => {
       renderTweets(tweets);
     });
   };
+  
   loadTweets();
 });
